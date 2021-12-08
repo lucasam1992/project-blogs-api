c668ca54aba7f8f19db8302cc1837f2e23f3f919
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-12-08 00:04:29 -0300

    Req 10 - endpoint put

---
+++ middlewares/authUserUpdate.js
@@ -0,0 +1,15 @@
+const rescue = require('express-rescue');
+const { BlogPost } = require('../models');
+
+module.exports = rescue(async (req, res, next) => {
+    const { id } = req.params;
+    const user = req.payload;
+    console.log(user);
+//    console.log(id);
+    const blog = await BlogPost.findByPk(id);
+   console.log(blog.dataValues.userId);
+    if (user.id !== blog.dataValues.userId) {
+        return res.status(401).json({ message: 'Unauthorized user' });
+    }
+    next();
+});
